2ad711975bf40fce8c29f6da1baec1111b4661da
Yongjin081002 <yg4059540@gmail.com>
2025-06-09 20:41:10 +0900

    feat: login page base

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from './style';
+import { createPortal } from 'react-dom';
+
+const Login = () => {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
